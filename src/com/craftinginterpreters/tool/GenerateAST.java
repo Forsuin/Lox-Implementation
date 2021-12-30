24b33e0f3829fd95102279b42217aff45b8b24a9
Ian Laidlaw <ian@kaboodles.com>
2021-12-29 23:15:22 -0500

    Added statements, expression and print, and set up build system to output jar file. Added test.txt which contains Lox code for testing. Notes also included

--- src/com/craftinginterpreters/tool/GenerateAST.java
+++ src/com/craftinginterpreters/tool/GenerateAST.java
@@ -14,11 +14,17 @@ public class GenerateAST
             System.exit(64);
         }
         String outputDir = args[0];
+        //generates AST of expression
         defineAst(outputDir, "Expr", Arrays.asList(
                 "Binary   : Expr left, Token operator, Expr right",
                 "Grouping : Expr expression",
                 "Literal  : Object value",
                 "Unary    : Token operator, Expr right"
+        ));
+        //generates AST of statements
+        defineAst(outputDir, "Stmt", Arrays.asList(
+                "Expression : Expr expression",
+                "Print      : Expr expression"
         ));
     }
 
