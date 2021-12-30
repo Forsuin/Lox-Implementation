23106fbc255225318698efc0e045b8ce676f92f7
Ian Laidlaw <ian@kaboodles.com>
2021-12-29 23:15:22 -0500

    Added statements, expression and print, and set up build system to output jar file. Added test.txt which contains Lox code for testing. Notes also included

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -58,12 +58,12 @@ public class Lox
         Lexer scanner = new Lexer(source);
         List<Token> tokens = scanner.scanTokens();
         Parser parser = new Parser(tokens);
-        Expr expression = parser.parse();
+        List<Stmt> statements = parser.parse();
 
         //stop if there was a syntax error
         if(hadError) return;
 
-        interpreter.interpret(expression);
+        interpreter.interpret(statements);
     }
 
     static void error(int line, String message){
