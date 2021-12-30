f964bd1454e4dbc9b5e3d1bb9aa64f02e4c3fe79
Ian Laidlaw <ian@kaboodles.com>
2021-12-29 19:56:10 -0500

    Added a parser, an interpreter, and support for runtime errors. Removed unnecessary import from Expr.java

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -10,8 +10,10 @@ import java.util.List;
 
 public class Lox
 {
-    static boolean hadError = false;
+    private static final Interpreter interpreter = new Interpreter();
 
+    static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException
     {
@@ -28,17 +30,21 @@ public class Lox
     }
 
     private static void runFile(String path) throws IOException{
+        //read source from file and execute it
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
 
         //Indicate error in exit code
         if(hadError) System.exit(65);
+        if(hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException{
+        //read from standard input
         InputStreamReader input = new InputStreamReader(System.in);
         BufferedReader reader = new BufferedReader(input);
 
+        //infinite prompt
         for(;;){
             System.out.print("> ");
             String line = reader.readLine();
@@ -51,10 +57,13 @@ public class Lox
     private static void run(String source){
         Lexer scanner = new Lexer(source);
         List<Token> tokens = scanner.scanTokens();
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
 
-        for(Token token : tokens){
-            System.out.println(token);
-        }
+        //stop if there was a syntax error
+        if(hadError) return;
+
+        interpreter.interpret(expression);
     }
 
     static void error(int line, String message){
@@ -64,5 +73,19 @@ public class Lox
     private static void report(int line, String where, String message){
         System.err.println("[line " + line + "] Error" + where + ": " + message);
         hadError = true;
+    }
+
+    static void error(Token token, String message){
+        if(token.type == TokenType.EOF){
+            report(token.line, " at end", message);
+        }
+        else{
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
+    }
+
+    static void runtimeError(RuntimeError error){
+        System.err.println(error.getMessage() + "\n[Line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 }
