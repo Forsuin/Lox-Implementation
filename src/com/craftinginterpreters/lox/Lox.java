7337f863be121be0016111c4daa678b84325d963
Ian Laidlaw <ian@kaboodles.com>
2022-01-03 22:01:15 -0500

    Fixed scope error where closures would refer to new definitions of variables of the same name. Added a resolver to check that that variable usage refers to the correct variable.

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -63,6 +63,11 @@ public class Lox
         //stop if there was a syntax error
         if(hadError) return;
 
+        Resolver resolver = new Resolver(interpreter);
+        resolver.resolve(statements);
+
+        if(hadError) return;
+
         interpreter.interpret(statements);
     }
 
