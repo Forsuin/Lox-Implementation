db0ddc4184042a8abfe9b23b7c821b5fb3772d77
Ian Laidlaw <ian@kaboodles.com>
2022-01-10 20:01:29 -0500

    Impl. chunks that hold bytecode and way to display it

--- clox/src/main.c
+++ clox/src/main.c
@@ -1,100 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "common.h"
+#include "chunk.h"
+#include "debug.h"
 
-struct Node
+int main(int argc, const char *argv[])
 {
-   struct Node *next;
-   struct Node *last;
-   char *value;
-};
+    Chunk chunk;
+    initChunk(&chunk);
 
-struct Node *head = NULL;
-struct Node *tail = NULL;
+    int constant = addConstant(&chunk, 1.2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
 
-// inserts new node at beginning
-void insert(char value[])
-{
-   // create new node
-   struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    writeChunk(&chunk, OP_RETURN, 123);
 
-   // set newNode's value and links
-   newNode->value = value;
-   newNode->next = head;
-   newNode->last = NULL;
-   if (head != NULL)
-      head->last = newNode;
-
-   // set beginning to the new node
-   head = newNode;
-}
-
-// deletes beginning node
-void deleteFirst()
-{
-   head = head->next;
-}
-
-void deleteNode(struct Node *toDelete)
-{
-   toDelete->last->next = toDelete->next;
-   toDelete->next->last = toDelete->last;
-
-   free(toDelete);
-}
-
-struct Node *popOff()
-{
-   struct Node *temp = head;
-
-   deleteFirst();
-
-   return temp;
-}
-
-struct Node *find(char valueToFind[])
-{
-   for (struct Node *i = head; i != tail; i = i->next)
-   {
-      if (strcmp(i->value, valueToFind) == 0)
-      {
-         return i;
-      }
-      else
-      {
-         continue;
-      }
-   }
-
-   return NULL;
-}
-
-void printList()
-{
-   for (struct Node *i = head; i != tail; i = i->next)
-   {
-      printf("%s\n", i->value);
-   }
-   printf("\n");
-}
-
-int main()
-{
-   insert("First");
-   insert("Second");
-   insert("Third");
-   insert("Red");
-   insert("Blue");
-
-   printList();
-
-   deleteNode(find("Third"));
-
-   printList();
-
-   printf("%s\n\n", popOff()->value);
-
-   printList();
-
-   return 0;
+    disassembleChunk(&chunk, "test chunk");
+    freeChunk(&chunk);
 }
