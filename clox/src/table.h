a094d36a15bed50947d944b3b41d1503984d6717
Ian Laidlaw <ian@kaboodles.com>
2022-01-16 19:31:01 -0500

    Added hash table for furture variable lookup and string interning

---
+++ clox/src/table.h
@@ -0,0 +1,30 @@
+#ifndef CLOX_TABLE_H
+#define CLOX_TABLE_H
+
+#include "common.h"
+#include "value.h"
+
+typedef struct
+{
+    ObjString *key;
+    Value value;
+} Entry;
+
+typedef struct
+{
+    int count;
+    int capacity;
+    Entry *entries;
+} Table;
+
+#define TABLE_MAX_LOAD 0.75
+
+void initTable(Table *table);
+void freeTable(Table *table);
+bool tableGet(Table *table, ObjString *key, Value *get);
+bool tableSet(Table *table, ObjString *key, Value value);
+bool tableDelete(Table *table, ObjString *key);
+void tableAddAll(Table *from, Table *to);
+ObjString *tableFindString(Table *table, const char *chars, int length, uint32_t hash);
+
+#endif
